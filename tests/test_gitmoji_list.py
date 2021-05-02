53f76bd68918db97b0f80499277547d9d98551dd
Marcelo Trylesinski <marcelotryle@gmail.com>
2021-05-02 16:51:32 +0200

    ✅ Fix tests

--- tests/test_gitmoji_list.py
+++ tests/test_gitmoji_list.py
@@ -1,11 +1,21 @@
 import subprocess
+from unittest import TestCase
 
 from gitmoji import GITMOJI_LIST_STR
 from gitmoji.constants import split_gitmoji_list
 
 
-def test_gitmoji_list():
-    subprocess.run(["npm", "install", "-g", "gitmoji-cli@latest"])
-    output = subprocess.getoutput("gitmoji --list")
-    print(output)
-    assert split_gitmoji_list(GITMOJI_LIST_STR) == split_gitmoji_list(output)
+class TestGitmojiList(TestCase):
+    def setUp(self):
+        subprocess.run(["npm", "install", "-g", "gitmoji-cli@latest"])
+        self.maxDiff = None
+
+    def test_gitmoji_list(self):
+        output = subprocess.getoutput("gitmoji --list")
+        if output.startswith("- Fetching the emoji list"):
+            output = "\n".join(output.splitlines()[2:])
+        for a, b in zip(
+            split_gitmoji_list(GITMOJI_LIST_STR), split_gitmoji_list(output)
+        ):
+            assert a["description"] == b["description"]
+            assert a["command"] == b["command"]
