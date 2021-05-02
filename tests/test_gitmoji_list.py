22d2733ce58a13eef69debc9548c6f9132650b92
Marcelo Trylesinski <marcelotryle@gmail.com>
2021-05-02 16:41:45 +0200

    🎉 First commit

---
+++ tests/test_gitmoji_list.py
@@ -0,0 +1,10 @@
+import subprocess
+
+from gitmoji import GITMOJI_LIST_STR
+from gitmoji.constants import split_gitmoji_list
+
+
+def test_gitmoji_list():
+    subprocess.run(["npm", "install", "-g", "gitmoji-cli@latest"])
+    output = subprocess.getoutput("gitmoji --list")
+    assert split_gitmoji_list(GITMOJI_LIST_STR) == split_gitmoji_list(output)
---
+++ tests/test_version.py
@@ -0,0 +1,5 @@
+import gitmoji
+
+
+def test_version():
+    assert gitmoji.__version__ == "0.1.0"
